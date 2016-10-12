bc7270b7263867cab102a2fc82b74b5aecb49edc
dsuare1 <suarez.derrick@gmail.com>
2016-10-11 22:52:59 -0500

    switched from nodemailer to mailgun

--- controllers/tra-central-controller.js
+++ controllers/tra-central-controller.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const nodemailer = require('nodemailer');
+
+const api_key = 'key-813d06ee3d1d178274bee4ceab8dab94';
+const domain = 'mg.theroundaboutstore.com';
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 const router = express.Router();
-const transporter = nodemailer.createTransport();
 
 const Albums = require('../models/Album.js');
 
@@ -36,17 +38,29 @@ router.get('/contact', (req, res) => {
 
 router.post('/contact', (req, res) => {
 
-	let mailOptions = {
-		from: req.body.email,
-		to: 'roundaboutstore@gmail.com',
-		subject: req.body.subject,
-		text: req.body.messageBody
+	// let mailOptions = {
+	// 	from: req.body.email,
+	// 	to: 'suarez.derrick@gmail.com',
+	// 	subject: req.body.subject,
+	// 	text: req.body.messageBody
+	// };
+
+	var data = {
+	  from: req.body.email,
+	  to: 'suarez.derrick@gmail.com',
+	  subject: req.body.subject,
+	  text: req.body.messageBody
 	};
 
-	transporter.sendMail(mailOptions, (err, info) => {
-		if (err) {
-			console.log(err);
-		};
+	// transporter.sendMail(mailOptions, (err, info) => {
+	// 	if (err) {
+	// 		console.log(err);
+	// 	};
+	// 	console.log(info);
+	// });
+
+	mailgun.messages().send(data, function (error, body) {
+	  console.log(body);
 	});
 
 	Albums.find({ isStaffPick: true }, (err, picks) => {
